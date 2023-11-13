7e1f1f8089d675ccc5905f45880711516536052a
Inajara <inawriedt@hotmail.com>
2023-11-13 17:46:29 -0300

    Pagina 63

---
+++ Capitulo.3/src/GradeBook.java
@@ -0,0 +1,6 @@
+public class GradeBook {
+    public void displayMessage( String courseName){
+        System.out.printf("Welcome to the Grade Book for\n%s!\n",
+                courseName);
+    }
+}
